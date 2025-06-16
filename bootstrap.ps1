5d28b29e65f39e5627051a57cedfde3441d0d465
Junfeng Lei <junf137@outlook.com>
2025-06-16 17:26:46 -0400

    [refactor] specify basedir

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,24 +4,17 @@
 # .\bootstrap.ps1
 
 # Define source and target file mappings
-$sourceFiles = @(
-    "git\gitconfig",
-    "vimfiles",
-    "vimfiles",
-    "conda\condarc",
-    "wezterm\wezterm-config",
-    "starship\starship-default",
-    "scoop"
-)
-
-$targetFiles = @(
-    ".gitconfig",
-    ".vim",
-    "vimfiles",
-    ".condarc",
-    ".config\wezterm\",
-    ".config\starship\",
-    ".config\scoop\"
+# Format: @{ Source = "relative/path/from/script"; Target = "target/path"; BaseDir = "ENV_VAR" }
+# If BaseDir is specified, Target will be joined with the value of that environment variable
+# If BaseDir is not specified, Target is treated as an absolute path
+$linkMappings = @(
+    @{ Source = "git\gitconfig"; Target = ".gitconfig"; BaseDir = "USERPROFILE" },
+    @{ Source = "vimfiles"; Target = ".vim"; BaseDir = "USERPROFILE" },
+    @{ Source = "vimfiles"; Target = "vimfiles"; BaseDir = "USERPROFILE" },
+    @{ Source = "conda\condarc"; Target = ".condarc"; BaseDir = "USERPROFILE" },
+    @{ Source = "wezterm\wezterm-config"; Target = ".config\wezterm"; BaseDir = "USERPROFILE" },
+    @{ Source = "starship\starship-default"; Target = ".config\starship"; BaseDir = "USERPROFILE" },
+    @{ Source = "scoop"; Target = ".config\scoop"; BaseDir = "USERPROFILE" }
 )
 
 # Function to create symbolic links
@@ -32,6 +25,13 @@ function New-SymbolicLink {
     )
 
     try {
+        # Create parent directory if it doesn't exist
+        $destinationDir = Split-Path -Path $destinationPath -Parent
+        if (!(Test-Path -Path $destinationDir)) {
+            New-Item -ItemType Directory -Path $destinationDir -Force | Out-Null
+            Write-Verbose "Created directory: $destinationDir"
+        }
+
         if (Test-Path $destinationPath) {
             # Backup or remove existing item
             $timestamp = Get-Date -Format "yyyyMMddHHmmss"
@@ -51,10 +51,18 @@ function New-SymbolicLink {
     }
 }
 
-# Link files to $env:USERPROFILE
-for ($i = 0; $i -lt $sourceFiles.Length; $i++) {
-    $sourcePath = Join-Path -Path $PSScriptRoot -ChildPath $sourceFiles[$i]
-    $destinationPath = Join-Path -Path $env:USERPROFILE -ChildPath $targetFiles[$i]
+# Process all symbolic links
+foreach ($mapping in $linkMappings) {
+    $sourcePath = Join-Path -Path $PSScriptRoot -ChildPath $mapping.Source
+
+    # Determine the destination path based on whether BaseDir is specified
+    if ($mapping.BaseDir) {
+        $baseDir = [Environment]::GetEnvironmentVariable($mapping.BaseDir)
+        $destinationPath = Join-Path -Path $baseDir -ChildPath $mapping.Target
+    }
+    else {
+        $destinationPath = $mapping.Target
+    }
 
     # Create symbolic link
     New-SymbolicLink -sourcePath $sourcePath -destinationPath $destinationPath
